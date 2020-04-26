c7a061dc25ffa76b8ea9987f75dcd767505cc3ff
Nguyen L. Vo <61761917+NguyenLeVo@users.noreply.github.com>
2020-04-26 19:55:13 -0400

    Update 2020-04-26 4.sql

--- Python/SQL/2020-04-26 4.sql
+++ Python/SQL/2020-04-26 4.sql
@@ -1,4 +1,5 @@
 /* In 4.sql, write a SQL query to determine the number of movies with an IMDb rating of 10.0. */
 
 SELECT COUNT(title) FROM movies
-WHERE id = (SELECT movie_id FROM ratings WHERE rating = 10);
+JOIN ratings ON movies.id = rating.movie_id
+WHERE rating = 10;
