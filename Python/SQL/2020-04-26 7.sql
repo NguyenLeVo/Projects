471da540da593bb7945f65ea1fe61baeadc9d9af
Nguyen L. Vo <61761917+NguyenLeVo@users.noreply.github.com>
2020-04-26 19:55:46 -0400

    Update 2020-04-26 7.sql

--- Python/SQL/2020-04-26 7.sql
+++ Python/SQL/2020-04-26 7.sql
@@ -2,9 +2,10 @@
 For movies with the same rating, order them alphabetically by title. */
 
 /* Don't have to create a new table from scratch, just specify the table and column*/
-SELECT movies.title, ratings.rating
-FROM movies JOIN ratings 
-WHERE movies.id = ratings.movie_id
+SELECT movies.title, ratings.rating FROM movies 
+JOIN ratings ON movie.id = ratings.movie_id
+WHERE year = 2010
 
 /* Set the order by rating descending, then for same rating, alphabetically by title*/
 ORDER BY rating DESC, title;
+
