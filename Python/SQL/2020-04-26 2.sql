842673285241e4d60adce1173f732f913cc02a48
Nguyen L. Vo <61761917+NguyenLeVo@users.noreply.github.com>
2020-04-26 17:47:05 -0400

    Update 2020-04-26 2.sql

--- Python/SQL/2020-04-26 2.sql
+++ Python/SQL/2020-04-26 2.sql
@@ -1,1 +1,4 @@
-SELECT birth FROM people WHERE name = 'Emma Stone';
+/* In 2.sql, write a SQL query to determine the birth year of Emma Stone. */
+
+SELECT birth FROM people 
+WHERE name = 'Emma Stone';
